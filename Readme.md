004d035027c1b3f45e40f800c6f7c955753fd759
Andrew Balanuk <abalanuk@lohika.com>
2017-05-11 17:28:46 +0300

    Webpack configs, babelRelayPlugin

---
+++ Readme.md
@@ -0,0 +1,4 @@
+Consider to take away of skeleton:
+- kit/entry/server.js
+- overall /lib folder and related files/deps
+- /webpack/server.js
---
+++ kit/webpack/scripts/babelRelayPlugin.js
@@ -0,0 +1,15 @@
+import getBabelRelayPlugin from 'babel-relay-plugin'
+import { introspectionQuery } from 'graphql/utilities'
+import request from 'sync-request'
+
+const url = 'http://localhost:3000/graphql'//192.168.56.1
+
+const response = request('POST', url, {
+  json: {
+    query: introspectionQuery,
+  },
+})
+
+const schema = JSON.parse(response.body.toString('utf-8'))
+//console.log(schema.data)
+module.exports = getBabelRelayPlugin(schema.data, { abortOnError: true })
